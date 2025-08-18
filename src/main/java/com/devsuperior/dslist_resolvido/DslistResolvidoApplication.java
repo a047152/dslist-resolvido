5ad11d59b1a8a0d0340685d4201ad4ff08534174
Felipe Castilho <a047152@umaia.pt>
2025-08-18 18:53:15 +0100

    Project created

---
+++ src/main/java/com/devsuperior/dslist_resolvido/DslistResolvidoApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dslist_resolvido;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DslistResolvidoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DslistResolvidoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsuperior/dslist_resolvido/DslistResolvidoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dslist_resolvido;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistResolvidoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
