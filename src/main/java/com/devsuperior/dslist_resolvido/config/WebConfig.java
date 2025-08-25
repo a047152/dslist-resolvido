4e0d190bb9f3d22607825ce54de76959022d9fd7
Felipe Castilho <a047152@umaia.pt>
2025-08-25 21:10:39 +0100

    Updates

--- src/main/java/com/devsuperior/dslist_resolvido/config/WebConfig.java
+++ src/main/java/com/devsuperior/dslist_resolvido/config/WebConfig.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import io.micrometer.common.lang.NonNull;
+
 @Configuration
 public class WebConfig {
 
@@ -16,7 +18,7 @@ public class WebConfig {
 	public WebMvcConfigurer corsConfigurer() {
 		return new WebMvcConfigurer() {
 			@Override
-			public void addCorsMappings(CorsRegistry registry) {
+			public void addCorsMappings(@NonNull CorsRegistry registry) {
 				registry.addMapping("/**").allowedMethods("*").allowedOrigins(corsOrigins);
 			}
 		};
